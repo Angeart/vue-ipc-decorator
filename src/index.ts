bb8afea04ebbc6a3b1d2c4815228f769270d2e73
Angeart <shinoyuki.angeart@gmail.com>
2019-10-24 17:08:40 +0900

    i guess its seems running

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { ipcRenderer } from 'electron-better-ipc';
+import { ipcRenderer, ipcMain } from 'electron-better-ipc';
 import { createDecorator } from 'vue-class-component';
 import Vue from 'vue';
+import { BrowserWindow } from 'electron';
 
 const _toKebabCaseRegex = /\B([A-Z])/g;
 const toKebabCase = (str: string) => str.replace(_toKebabCaseRegex, '-$1').toLowerCase();
@@ -28,23 +29,63 @@ export function IpcRendererSend(event?: string) {
  */
 export function IpcRendererReceive(event?: string) {
     return createDecorator((componentOptions, key) => {
+        if (!componentOptions.methods) {
+            return
+        }
+        const origin = componentOptions.methods[key]
         event = event ? toKebabCase(event): undefined;
         key = toKebabCase(key);
-        const origin = componentOptions.provide as Function;
+        console.dir(componentOptions)
         const originMounted = componentOptions.mounted || (() => {});
-        // componentOptions.mounted = function (this: Vue) {
-        //     console.log('mounted')
-        //     ipcRenderer.answerMain(event || key, (...args: any[]) => {
-        //         origin.apply(this, args)
-        //     });
-        // }
-        componentOptions.mounted = pipe(originMounted, function (this: Vue) {
-            console.log('mounted')
+        componentOptions.mounted = pipe(function (this: Vue) {
+            console.log('ipc receive mounted')
             ipcRenderer.answerMain(event || key, (...args: any[]) => {
-                origin.apply(this, args)
+                console.dir(origin)
+                return origin.apply(this, args)
             });
-        })
-        componentOptions.provide = function () {}
+        }, originMounted)
     })
 }
 
+/**
+ * decorator of ipcMain Send function
+ * @param event the name of event, default used by function name
+ * @return Method Decorator
+ */
+export function IpcMainSend(event?: string) {
+    return function(_target: Object, key: string, descripter: PropertyDescriptor) {
+        event = event ? toKebabCase(event): undefined;
+        key = toKebabCase(key);
+        const origin = descripter.value as Function;
+        descripter.value = async function (...args: any[]) {
+            const window = BrowserWindow.getFocusedWindow();
+            if (!window) {
+                console.error('vue-ipc-decorator','cannot get the focused window');
+                return;
+            }
+            return ipcMain.callRenderer(window, event || key, origin.apply(this, args));
+        }
+    }
+}
+
+/**
+ * decorator of ipcMain Send function
+ * @param event the name of event, default used by function name
+ * @return Method Decorator
+ */
+export function IpcMainReceive(event?: string) {
+    return function(target: Object, key: string, descripter: PropertyDescriptor) {
+        event = event ? toKebabCase(event): undefined;
+        key = toKebabCase(key);
+        const origin = descripter.value as Function;
+        console.log('ipc main receive decorator')
+        console.dir(target.constructor)
+        const register = function (this: typeof target) {
+            console.log('registered')
+            ipcMain.answerRenderer(event || key, (...args: any[]) => {
+                return origin.apply(this, args)
+            });
+        }
+        register.apply(target);
+    }
+}
