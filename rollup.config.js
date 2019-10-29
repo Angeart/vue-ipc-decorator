3e0a84e64aed365f99c0c05df039bfb070bf7aca
Angeart <shinoyuki.angeart@gmail.com>
2019-10-29 17:20:30 +0900

    strip console.log

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,28 @@
+import strip from 'rollup-plugin-strip';
 export default {
     input: 'lib/index.js',
-    output: {
-        file: 'lib/index.umd.js',
-        format: 'umd',
-        name: 'IpcDecorator',
-        globals: {
-            'electron-better-ipc': 'ElectronBetterIpc'
+    output: [
+        {
+            file: 'lib/index.umd.js',
+            format: 'umd',
+            name: 'IpcDecorator',
+            globals: {
+                'electron-better-ipc': 'ElectronBetterIpc'
+            },
+            exports: 'named'
         },
-        exports: 'named'
-    },
-    external: ['electron-better-ipc', 'reflect-metadata']
+        {
+            file: 'lib/index.js',
+            format: 'esm',
+            name: 'IpcDecorator',
+            globals: {
+                'electron-better-ipc': 'ElectronBetterIpc'
+            },
+            exports: 'named'
+        },
+    ],
+    external: ['electron-better-ipc', 'reflect-metadata'],
+    plugins: [
+        strip()
+    ]
 }
